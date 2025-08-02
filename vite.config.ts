059f8c54962152aba257152b501f2b6a03af5f78
Lucas Cid <lucasfelipaaa@gmail.com>
2025-08-02 13:19:41 -0300

    refactor/chore: now using latest version of tailwind and deleting unused files

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    tailwindcss(),    
+    tsconfigPaths(),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
