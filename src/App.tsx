937896c0b6121e960ba63fb225dd44ca24f58f15
Lucas Cid <lucasfelipaaa@gmail.com>
2025-08-13 17:37:18 -0300

    feat: add queryclient provider and client to initiate requests cached

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
-import { useState } from 'react';
-import { SEOHead } from './components/SEOHead';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from './components/ThemeProvider';
-import { ThemeToggle } from './components/ThemeToggle';
-import { Header } from './components/Header';
-import { Hero } from './components/Hero';
-import { Features } from './components/Features';
-import { Pricing } from './components/Pricing';
-import { Footer } from './components/Footer';
-import { Downloads } from './components/Downloads';
-import { Demo } from './components/Demo';
 import { Router } from './routes/index.routes';
+import { useMemo } from 'react';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState<'home' | 'downloads'>('home');
-
-  const handleDownloadsClick = () => {
-    setCurrentPage('downloads');
-  };
-
-  const handleBackToHome = () => {
-    setCurrentPage('home');
-  };
-
+  const queryClient = useMemo(() => (new QueryClient()), []);
   return (
-    <ThemeProvider>
-      <Router />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <Router />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
